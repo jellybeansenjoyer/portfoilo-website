dcd54cc4e3b48de66f5d60519736d4c2893b6493
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2024-01-13 15:47:06 +0530

    loading animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,8 +16,14 @@ const Hero = () => {
             <p className={`${styles.heroSubText} mt-2 text-white-100`}>{texts.description}</p> 
           </div>
         </div>
-          <ComputersCanvas/>
-         
+          <ComputersCanvas />
+          <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+            <a href="#about">
+              <div className='w-[32px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+                <motion.dev animate={{y:[0,24,0] }} transition={{duration:1.5,repeat:Infinity,repeatType:'loop'}} className="w-3 h-3 rounded-full bg-secondary mb-1" />
+              </div>
+            </a>
+          </div>
     </section>
   )
 }
